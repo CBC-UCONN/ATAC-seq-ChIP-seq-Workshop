1a0f610b7d0ad6dfa747a24bcf418cd61ebb62e5
Kerry Cobb <cobbkerry@gmail.com>
2025-10-27 18:43:21 -0400

    Remove some scripts, add symlinking of resources

---
+++ resources/symlink_all.sh
@@ -0,0 +1,7 @@
+ln -s /core/cbc/tutorials/workshopdirs/Chip-ATAC/resources/JASPAR2024_CORE_vertebrates_non-redundant_pfms_jaspar.txt .
+ln -s /core/cbc/tutorials/workshopdirs/Chip-ATAC/resources/Mus_musculus.GRCm39.dna_rm.primary_assembly_bwa .
+ln -s /core/cbc/tutorials/workshopdirs/Chip-ATAC/resources/Mus_musculus.GRCm39.dna_rm.primary_assembly_bwa2 .
+ln -s /core/cbc/tutorials/workshopdirs/Chip-ATAC/resources/Mus_musculus.GRCm39.dna_rm.primary_assembly.fa .
+ln -s /core/cbc/tutorials/workshopdirs/Chip-ATAC/resources/Mus_musculus.GRCm39.dna_rm.primary_assembly.fa.fai .
+ln -s /core/cbc/tutorials/workshopdirs/Chip-ATAC/resources/Mus_musculus.GRCm39.dna_rm.primary_assembly_sizes.txt .
+ln -s /core/cbc/tutorials/workshopdirs/Chip-ATAC/resources/nf-xanadu.config .
