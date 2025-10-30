f11baf61d1f9093ca407c3728ac03d989f539292
Kerry Cobb <cobbkerry@gmail.com>
2025-10-29 23:54:28 -0400

    Fix chip qc

--- dmrt/chip-seq/scripts/11_chip_qc.R
+++ dmrt/chip-seq/scripts/11_chip_qc.R
@@ -1,45 +1,13 @@
-
-## Load libraries
 library(ChIPQC)
 
-# Setup
-out_dir <- "../results/11_chip_qc/"
-dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
+args <- commandArgs(trailingOnly=TRUE)
+filtered_bam <- args[1]
+peaks <- args[2]
+outdir <- args[3]
 
-meta_df <- read.csv("../meta/chip-sra-meta.csv")
-meta_df <- meta_df[1:4,]
-meta_df
-bam_files <- file.path("../results/08_filter", paste0(meta_df$Run, ".filtered.sorted.bam"))
-peak_files <- file.path("../results/10_macs", paste0(meta_df$Run, "_peaks.narrowPeak"))
+# Extract sample name from BAM file path
+name <- gsub(".sorted.bam", "", basename(args[1]))
 
-stopifnot(all(file.exists(bam_files)))
-stopifnot(all(file.exists(peak_files)))
+sample <- ChIPQCsample(filtered_bam)
 
-sample_sheet <- data.frame(
-  SampleID = meta_df$Run,
-  Condition = meta_df$source_name,
-  bamReads = bam_files,
-  Peaks = peak_files,
-  PeakCaller = "macs",
-  PeakFormat = "narrow")
-
-## Create ChIPQC object
-chipObj <- ChIPQC(sample_sheet, chromosomes=NULL) 
-
-## Create ChIPQC report
-ChIPQCreport(chipObj, reportName="ChIP_QC_report", reportFolder=outdir)
-
-# View summary statistics
-chipqc_obj
-
-# Access specific QC metrics
-QCmetrics(chipqc_obj)
-
-# Plot quality metrics
-plotCC(chipqc_obj)  # Cross-coverage plot
-plotRegi(chipqc_obj)  # Reads in genomic regions
-plotFrip(chipqc_obj)  # Fraction of reads in peaks
-
-# Export QC metrics to CSV
-qc_metrics <- QCmetrics(chipqc_obj)
-write.csv(qc_metrics, file.path(out_dir, "qc_metrics.csv"), row.names = FALSE)
+ChIPQCreport(sample, peaks, reportName=paste0(name, "_ChIP_QC_report"), reportFolder=outdir)
--- dmrt/chip-seq/scripts/11_chip_qc.sh
+++ dmrt/chip-seq/scripts/11_chip_qc.sh
@@ -4,7 +4,8 @@
 #SBATCH --mem=32G
 #SBATCH --partition=general
 #SBATCH --qos=general
-#SBATCH --output=logs/%x_%j.out
+#SBATCH --output=logs/%x_%A_%a.out
+#SBATCH --array=1-12
 
 echo "Job running on: $(hostname)"
 start=$(date +%s)
@@ -13,8 +14,26 @@ echo "Start time: $(date)"
 source ~/.bashrc
 mamba activate r_atac
 
+# Store some paths as variables
+meta_data=../meta/chip-sra-meta.csv
+filtered=../results/08_filter
+peaks=../results/10_macs
+
+# Create output directory if it doesn't exist 
+
+# Get Nth SRA run accession based on SLURM_ARRAY_TASK_ID from metadata csv file
+sample=$(awk -F, -v row=${SLURM_ARRAY_TASK_ID} \
+    'NR==1{for(i=1;i<=NF;i++)if($i=="Run")col=i}NR==row+1&&col{print $col}' $meta_data)
+
+outdir=../results/11_chip_qc/$sample
+mkdir -p $outdir
+
 # Run QC R script
-Rscript 11_chip_qc.R
+Rscript 11_chip_qc.R \
+  $filtered/$sample.filtered.sorted.bam \
+  $peaks/${sample}_peaks.narrowPeak \
+  $outdir
+
 
 echo "End time: $(date)"
 echo "Elapsed time: $(date -ud "@$(($(date +%s)-start))" +'%H hr %M min %S sec')"
